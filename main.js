43fc69510efed72b5cc9a5f3b39c7370dd289d1b
Patrick Okafor Chibuike <Patrickpeters911@gmail.com>
2023-01-19 00:54:36 +0100

    added a fancy bolt lightening

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ function createSnowFlakes() {
     const snowflake = document.createElement('i');
     snowflake.classList.add('fa-regular'); 
     snowflake.classList.add('fa-snowflake');
-    snowflake.style.left = Math.random() * window.innerWidth + 'px'; 
+    
+    snowflake.style.left = Math.random() * window.innerWidth + 'px';  
     snowflake.style.fontSize = Math.random() * 5 * 3 + 'px';
-    snowflake.style.top =  Math.random() * innerHeight + 'px';
     snowflake.style.opacity = Math.random(); 
+
     snowflake.addEventListener('mouseover', function(){
         this.style.visibility = 'hidden'
     })
@@ -15,9 +16,30 @@ function createSnowFlakes() {
     document.body.append(snowflake); 
     setTimeout(() => {
         snowflake.remove();
-    }, 4000);
+    }, 6000);
            
 } 
 
-setInterval(createSnowFlakes, 70) ;
+setInterval(createSnowFlakes, 70) ;    
 
+
+function createLightening() {
+    const createLightening = document.createElement('i');
+
+    createLightening.classList.add('fa-solid');
+    createLightening.classList.add('fa-bolt-lightning');
+
+    createLightening.style.left = Math.random() * window.innerWidth + 'px';  
+    createLightening.style.top = Math.floor(Math.random() * 80 + 20) + 'px' ;       
+    createLightening.style.opacity =  Math.random();
+
+    setTimeout(() => {
+        createLightening.remove();
+    }, 3000);
+                               
+
+    document.body.append(createLightening)
+    
+}
+
+setInterval(createLightening, 6000)
